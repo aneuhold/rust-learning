1ab2746f5de00f5467e7abf5e5fdd24e50ff04d2
Anton G. Neuhold Jr <agneuhold@gmail.com>
2023-05-07 15:06:31 -0700

    Initial commit

---
+++ src/basicFileReading.rs
@@ -0,0 +1,69 @@
+use rand::prelude::*;
+
+/// Returns true `1` in `denominator` times.
+fn one_in(denominator: u32) -> bool {
+    thread_rng().gen_ratio(1, denominator)
+}
+
+#[derive(Debug)]
+struct File {
+    name: String,
+    data: Vec<u8>,
+}
+
+impl File {
+    fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
+        }
+    }
+
+    fn new_with_data(name: &str, data: &Vec<u8>) -> File {
+        let mut f = File::new(name);
+        f.data = data.clone();
+        f
+    }
+
+    fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> {
+        let mut tmp = self.data.clone();
+        let read_length = tmp.len();
+
+        save_to.reserve(read_length);
+        save_to.append(&mut tmp);
+        Ok(read_length)
+    }
+}
+
+fn open(f: File) -> Result<File, String> {
+    if one_in(10_000) {
+        let err_msg = String::from("Permission denied");
+        return Err(err_msg);
+    }
+    Ok(f)
+}
+
+fn close(f: File) -> Result<File, String> {
+    if one_in(100_000) {
+        let err_msg = String::from("Interrupted by signal!");
+        return Err(err_msg);
+    }
+    Ok(f)
+}
+
+fn main() {
+    let f3_data: Vec<u8> = vec![114, 117, 115, 116, 33];
+    let mut f3 = File::new_with_data("3.txt", &f3_data);
+
+    let mut buffer: Vec<u8> = vec![];
+
+    f3 = open(f3).unwrap();
+    let f3_length = f3.read(&mut buffer).unwrap();
+    f3 = close(f3).unwrap();
+
+    let text = String::from_utf8_lossy(&buffer);
+
+    println!("{:?}", f3);
+    println!("{} is {} bytes long", &f3.name, f3_length);
+    println!("{}", text);
+}
